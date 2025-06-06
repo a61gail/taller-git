f4eaa64c0e4d50422ac4da4a622cc00baccf0178
Ulises Juarez <mmcjuarez@gmail.com>
2025-06-05 19:27:31 -0600

    Hola abi hice algunos cambios

--- hola_mundo.c
+++ hola_mundo.c
@@ -2,11 +2,12 @@
 
 #include <stdio.h>
 int main(){
-printf("Hola Mundo, ¿cómo estás?\n");
+    printf("Hola Mundo, ¿cómo estás?\n");
+    int fact=factorial(20);
+    printf("El factorial de 20 es %d",fact):
+}
 
 int factorial(int n){
-    if (n==0) return 1;
-    return n * factorial(n-1);
-}
-
-}
+        if (n==0) return 1;
+        return n * factorial(n-1);
+    }   
