bedce07b7057b0f3f0f9dea56639ae6f7a1f40cd
Ulises Juarez <mmcjuarez@gmail.com>
2025-06-05 19:58:17 -0600

    Ya estan los cambios realizados, ya realiza factorial para cualquier numero

--- hola_mundo.c
+++ hola_mundo.c
@@ -1,13 +1,19 @@
 //Autor: Abigail Rios
 
 #include <stdio.h>
-int main(){
-    printf("Hola Mundo, ¿cómo estás?\n");
-    int fact=factorial(20);
-    printf("El factorial de 20 es %d",fact):
-}
 
 int factorial(int n){
         if (n==0) return 1;
         return n * factorial(n-1);
     }   
+
+int main(){
+    printf("Hola Mundo, ¿cómo estás?\n");
+    printf("Ingresa un numero para calcular su factorial por favor\n");
+    int n=0;
+    scanf("%d",&n);
+    int fact=factorial(n);
+    printf("El factorial de %d es %d\n",n,fact);
+    
+    return 0;
+}
